1e460d3f1a75f7c2d22a445e7d84b35fc6da5cfd
uday <admin@cloudtrainer.in>
2021-11-07 17:08:47 +0530

    Changed the region to UK South

--- main.tf
+++ main.tf
@@ -16,6 +16,6 @@ provider "azurerm" {
 #Resource Group creation
 resource "azurerm_resource_group" "basicrg" {
   name     = "${var.environment}-basic-rg"
-  location = "East US"
+  location = "UK South"
   tags = var.tags
 }
